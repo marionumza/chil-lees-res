bbe9001c64bd4d6159d08ccdf16a88bd45dfd531
fatmanedal <fatma-nedal@hotmail.com>
2021-10-23 15:08:00 +0200

    [ANF] add  late to attendance sheet

--- employee_late_check_in/models/hr_attendance.py
+++ employee_late_check_in/models/hr_attendance.py
@@ -37,23 +37,26 @@ class HrAttendance(models.Model):
 
             rec.late_check_in = 0.0
             if rec.sudo().check_in:
+                print(rec.check_in,';;;;')
                 week_day = rec.sudo().check_in.weekday()
                 if rec.employee_id.contract_id:
                     work_schedule = rec.sudo().employee_id.contract_id.resource_calendar_id
                     for schedule in work_schedule.sudo().attendance_ids:
-                        if schedule.dayofweek == str(week_day) and schedule.day_period == 'morning':
-                            work_from = schedule.hour_from
+                        if   schedule.day_period == 'morning':
+                            work_from = schedule.hour_from+5
+                            print(work_from)
                             result = '{0:02.0f}:{1:02.0f}'.format(*divmod(work_from * 60, 60))
 
                             user_tz = self.env.user.tz
+                            print(user_tz,'lllllll')
                             dt = rec.check_in
 
-                            if user_tz in pytz.all_timezones:
-                                old_tz = pytz.timezone('UTC')
-                                new_tz = pytz.timezone(user_tz)
-                                dt = old_tz.localize(dt).astimezone(new_tz)
+                            dt = rec.check_in + timedelta(hours=2)
+                            print(dt,'ddd')
                             str_time = dt.strftime("%H:%M")
+                            print(str_time,'str_time')
                             check_in_date = datetime.strptime(str_time, "%H:%M").time()
+
                             start_date = datetime.strptime(result, "%H:%M").time()
                             t1 = timedelta(hours=check_in_date.hour, minutes=check_in_date.minute)
                             t2 = timedelta(hours=start_date.hour, minutes=start_date.minute)
