2ea6cc6505f8c5a42e1ff858b7d7a75055c9cd36
fatmanedal <fatma-nedal@hotmail.com>
2021-10-23 10:38:22 +0200

    [ANF] add  late to attendance sheet

--- attendace_sheet_report/models/models.py
+++ attendace_sheet_report/models/models.py
@@ -8,6 +8,8 @@ class AttendanceInherit(models.Model):
 
     work_from = fields.Float(string="Work From",  required=False,compute="_compute_work_from" )
     work_to = fields.Float(string="Work To",  required=False,compute="_compute_work_to" )
+    # late = fields.Float(string="Late",  required=False, )
+    late = fields.Float(string="Late",  required=False,compute="_compute_late" )
 
 
     @api.depends('work_from')
@@ -26,3 +28,19 @@ class AttendanceInherit(models.Model):
             for i in attend:
                 rec.work_to = i.hour_to
 
+
+    @api.depends('work_from')
+    def _compute_late(self):
+        from1=0
+        work_from_to_time = 0.0
+        for rec in self:
+
+            check_in_float = float(str(rec.check_in.hour)+'.'+str(rec.check_in.minute))
+            print(check_in_float)
+            print(rec.work_from  )
+            if check_in_float>rec.work_from:
+
+                print(check_in_float-rec.work_from)
+                rec.late=check_in_float-rec.work_from
+            else:
+                rec.late=0
