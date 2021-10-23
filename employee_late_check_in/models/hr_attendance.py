54434d7d597dcee1ff931faeca19766e2a9590b6
fatmanedal <fatma-nedal@hotmail.com>
2021-10-23 15:46:51 +0200

    [ANF] add  late to attendance sheet

--- employee_late_check_in/models/hr_attendance.py
+++ employee_late_check_in/models/hr_attendance.py
@@ -59,7 +59,7 @@ class HrAttendance(models.Model):
                             dt = rec.check_in
 
                             _logger.info(dt,'dt')
-                            dt = rec.check_in + timedelta(hours=4)
+                            dt = rec.check_in + timedelta(hours=2)
                             _logger.info(dt,'ddd')
                             _logger.info('----')
                             str_time = dt.strftime("%H:%M")
