6771f0f626607e03f93687b8f7d3c28602667620
fatmanedal <fatma-nedal@hotmail.com>
2021-10-23 13:44:51 +0200

    [ANF] add  late to attendance sheet

--- employee_late_check_in/models/hr_attendance.py
+++ employee_late_check_in/models/hr_attendance.py
@@ -36,29 +36,30 @@ class HrAttendance(models.Model):
         for rec in self:
 
             rec.late_check_in = 0.0
-            week_day = rec.sudo().check_in.weekday()
-            if rec.employee_id.contract_id:
-                work_schedule = rec.sudo().employee_id.contract_id.resource_calendar_id
-                for schedule in work_schedule.sudo().attendance_ids:
-                    if schedule.dayofweek == str(week_day) and schedule.day_period == 'morning':
-                        work_from = schedule.hour_from
-                        result = '{0:02.0f}:{1:02.0f}'.format(*divmod(work_from * 60, 60))
+            if rec.sudo().check_in:
+                week_day = rec.sudo().check_in.weekday()
+                if rec.employee_id.contract_id:
+                    work_schedule = rec.sudo().employee_id.contract_id.resource_calendar_id
+                    for schedule in work_schedule.sudo().attendance_ids:
+                        if schedule.dayofweek == str(week_day) and schedule.day_period == 'morning':
+                            work_from = schedule.hour_from
+                            result = '{0:02.0f}:{1:02.0f}'.format(*divmod(work_from * 60, 60))
 
-                        user_tz = self.env.user.tz
-                        dt = rec.check_in
+                            user_tz = self.env.user.tz
+                            dt = rec.check_in
 
-                        if user_tz in pytz.all_timezones:
-                            old_tz = pytz.timezone('UTC')
-                            new_tz = pytz.timezone(user_tz)
-                            dt = old_tz.localize(dt).astimezone(new_tz)
-                        str_time = dt.strftime("%H:%M")
-                        check_in_date = datetime.strptime(str_time, "%H:%M").time()
-                        start_date = datetime.strptime(result, "%H:%M").time()
-                        t1 = timedelta(hours=check_in_date.hour, minutes=check_in_date.minute)
-                        t2 = timedelta(hours=start_date.hour, minutes=start_date.minute)
-                        if check_in_date > start_date:
-                            final = t1 - t2
-                            rec.sudo().late_check_in = final.total_seconds() / 60
+                            if user_tz in pytz.all_timezones:
+                                old_tz = pytz.timezone('UTC')
+                                new_tz = pytz.timezone(user_tz)
+                                dt = old_tz.localize(dt).astimezone(new_tz)
+                            str_time = dt.strftime("%H:%M")
+                            check_in_date = datetime.strptime(str_time, "%H:%M").time()
+                            start_date = datetime.strptime(result, "%H:%M").time()
+                            t1 = timedelta(hours=check_in_date.hour, minutes=check_in_date.minute)
+                            t2 = timedelta(hours=start_date.hour, minutes=start_date.minute)
+                            if check_in_date > start_date:
+                                final = t1 - t2
+                                rec.sudo().late_check_in = final.total_seconds() / 60
 
     def late_check_in_records(self):
         existing_records = self.env['late.check_in'].sudo().search([]).attendance_id.ids
